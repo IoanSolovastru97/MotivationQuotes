6ecba7330a01b79c50da62fc14615e325611be0b
Solovastru Ioan <nelusolovastru@gmail.com>
2018-10-14 15:52:04 +0300

    Connection succesful, When switching some data is not transfered

--- app/src/main/java/nelu/com/motivationquotes/GymActivity.java
+++ app/src/main/java/nelu/com/motivationquotes/GymActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class GymActivity extends AppCompatActivity {
+public class GymActivity extends MainActivity {
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
--- app/src/main/java/nelu/com/motivationquotes/MainActivity.java
+++ app/src/main/java/nelu/com/motivationquotes/MainActivity.java
@@ -1,15 +1,20 @@
 package nelu.com.motivationquotes;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,21 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView randomImage;
     private DrawerLayout mainLayoutDrawer;
     private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Button mainButton = findViewById(R.id.main);
-//        final TextView text = findViewById(R.id.textMain);
-//        mainButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent openGym = new Intent("app.java.nelu.com.motivationquotes.GymActivity");
-//                startActivity(openGym);
-//                text.setText("Button clicked");
-//            }
-//        });
 
         //Setting the upper left button
         mainLayoutDrawer = findViewById(R.id.drawerlayout); //Main Activity layout
@@ -41,10 +37,29 @@ public class MainActivity extends AppCompatActivity {
         mainLayoutDrawer.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        setupNavigationView();
 
         generateRandomImage();
 
+    }
+
+    private void setupNavigationView() {
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.quotesGym:
+                        startActivity(new Intent(MainActivity.this, GymActivity.class));
+                        break;
+                    case R.id.quotesFun:
+
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     private void generateRandomImage() {
@@ -58,5 +73,15 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Nu stiu ce face, merge si fara
+    @Override
+    public void onBackPressed() {
+        if (mainLayoutDrawer.isDrawerOpen(GravityCompat.START)) {
+            mainLayoutDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
